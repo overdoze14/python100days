68dc18bf92cd0ae6454cad997e39e729eb072b52
Alexander Alexandrov <alexander_alexandrov4@epam.com>
2022-03-01 09:05:23 +0300

    Day3: Love calc - combined string

--- Day3/love_calculator.py
+++ Day3/love_calculator.py
@@ -2,31 +2,20 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-name1 = name1.lower()
-name2 = name2.lower()
+combined_string = name1 + name2
+combined_string = combined_string.lower()
 
-t = name1.count("t")
-r = name1.count("r")
-u = name1.count("u")
-e = name1.count("e")
-
-
-t += name2.count("t")
-r += name2.count("r")
-u += name2.count("u")
-e += name2.count("e")
+t = combined_string.count("t")
+r = combined_string.count("r")
+u = combined_string.count("u")
+e = combined_string.count("e")
 
 x = t + r + u + e
 
-l = name1.count("l")
-o = name1.count("o")
-v = name1.count("v")
-e = name1.count("e")
-
-l += name2.count("l")
-o += name2.count("o")
-v += name2.count("v")
-e += name2.count("e")
+l = combined_string.count("l")
+o = combined_string.count("o")
+v = combined_string.count("v")
+e = combined_string.count("e")
 
 y = l + o + v + e
 
